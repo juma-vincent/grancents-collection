a06c29e9d8804b3d152b085ac185beba6d727459
Vincent Juma <61782453+juma-vincent@users.noreply.github.com>
2020-07-30 22:06:42 +0300

    replaced redux thunk with redux saga on for shop. Added a root saga

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -17,18 +17,3 @@ export const fetchCollectionsFailure = (errorMessage) => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
   payload: errorMessage,
 });
-
-export const fetchCollectionsStartAsync = () => {
-  return (dispatch) => {
-    const collectionRef = firestore.collection(`collections`);
-    dispatch(fetchCollectionsStart());
-
-    collectionRef
-      .get()
-      .then(async (snapShot) => {
-        const collectionsMap = convertCollectionSnapshotToMap(snapShot);
-        dispatch(fetchCollectionsSuccess(collectionsMap));
-      })
-      .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
-  };
-};
---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,28 @@
+import { takeLatest, call, put } from "redux-saga/effects";
+import {
+  convertCollectionSnapshotToMap,
+  firestore,
+} from "./../../firebase/firebase.utils";
+import ShopActionTypes from "./shop.types";
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from "./shop.actions";
+
+export function* fetchCollectionsAsync() {
+  try {
+    const collectionRef = firestore.collection(`collections`);
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(convertCollectionSnapshotToMap, snapshot);
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+}
+
+export function* fetchCollectionsStart() {
+  yield takeLatest(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
+}
