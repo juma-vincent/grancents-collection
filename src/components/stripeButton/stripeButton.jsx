adc5052fce425432d0c55248a956be00699d0850
Vincent Juma <61782453+juma-vincent@users.noreply.github.com>
2020-08-05 19:55:57 +0300

    deleted axios importation from stripe button component

--- src/components/stripeButton/stripeButton.jsx
+++ src/components/stripeButton/stripeButton.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
-import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
