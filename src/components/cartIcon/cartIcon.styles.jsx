b0ecb8705a7fc9d1b6442bf64252a96ddd79bb1c
juma-vincent <vinsoncode@gmail.com>
2020-08-10 05:59:24 +0300

    added styles to cart icon component for to enhance mobile responsiveness

--- src/components/cartIcon/cartIcon.styles.jsx
+++ src/components/cartIcon/cartIcon.styles.jsx
@@ -9,6 +9,14 @@ export const CartIconContainer = styled.div`
   align-items: center;
   justify-content: center;
   cursor: pointer;
+  @media screen and (max-width: 800px) {
+    width: 35px;
+    height: 35px;
+  }
+  @media screen and (max-width: 400px) {
+    width: 25px;
+    height: 25px;
+  }
 `;
 
 export const ItemCountContainer = styled.span`
@@ -18,9 +26,21 @@ export const ItemCountContainer = styled.span`
   right: 22px;
   bottom: 30px;
   color: rgb(236, 101, 11);
+  @media screen and (max-width: 800px) {
+    right: 16.5px;
+    bottom: 22px;
+  }
+  @media screen and (max-width: 400px) {
+    right: 12px;
+    bottom: 17px;
+  }
 `;
 
 export const ShoppingIcon = styled(ShoppingIconSVG)`
   width: 35px;
   height: 35px;
+  @media screen and (max-width: 800px) {
+    width: 29px;
+    height: 29px;
+  }
 `;
